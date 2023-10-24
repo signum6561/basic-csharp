4681b62a912ce4b93e691f1a5516ebdaadf609ab
signum6561 <quiocdat777@gmail.com>
2023-10-24 20:52:24 +0700

    fix

---
+++ InputOuput/3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        System.Console.Write("Nhập năm sinh: ");
+        int namSinh = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Số tuổi của người đó: " + (2023 - namSinh));
+    }
+}

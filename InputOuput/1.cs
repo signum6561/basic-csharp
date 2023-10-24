beb2ee901df9f769c1499f1e6de5c558669eca70
signum6561 <quiocdat777@gmail.com>
2023-10-24 20:29:33 +0700

    fix

---
+++ InputOuput/1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        System.Console.Write("Nhập họ và tên: ");
+        string? hoTen = Console.ReadLine();
+        System.Console.WriteLine("Họ tên: " + hoTen);
+    }
+}

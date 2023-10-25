8a651baa70d87e8444b11322359a888c936af80f
signum6561 <quiocdat777@gmail.com>
2023-10-25 19:01:45 +0700

    update

---
+++ Operator/5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        double canhKe, canhDoi;
+        double canhHuyen;
+        System.Console.Write("Nhập vào cạnh kề: ");  
+        canhKe = Convert.ToDouble(Console.ReadLine());
+        System.Console.Write("Nhập vào cạnh đối: ");  
+        canhDoi = Convert.ToDouble(Console.ReadLine());
+        
+        canhHuyen = Math.Sqrt(Math.Pow(canhKe, 2) + Math.Pow(canhDoi, 2));
+        
+        System.Console.WriteLine("Cạnh huyền là: " + canhHuyen);
+    }
+}

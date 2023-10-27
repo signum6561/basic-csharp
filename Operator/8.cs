fddab0418b5c92b63f01ba4976ac42365b46d8ed
signum6561 <quiocdat777@gmail.com>
2023-10-27 09:58:22 +0700

    update

---
+++ Operator/8.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        // n là số thực
+        // n [0, 0.5) => 0
+        // n [0.5, 1) => 1
+        // 0.5
+        // 1.2
+        // 1
+        // 1.7 => 2
+
+        System.Console.Write("Nhập vào n: ");
+        double n = Convert.ToDouble(Console.ReadLine());
+
+        // n += 0.5;
+        // double kq = n - (n % 1);
+
+        // int kq = (int)(n + 0.5);
+
+        double kq = Math.Round(n);
+        System.Console.Write("Kết quả: " + kq);
+    }
+}
---
+++ Operator/9.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        /*
+        Dữ kiện hữu hình:
+        m: số nguyên
+        50, 20, 10, 5, 2, 1
+        Output 6 số => 6 biến
+        197
+        50 + 50 + 50
+        197 - 150 = 47
+        197 % 50 = 47
+        20 + 20
+        47 - 40 = 7
+        5
+        7 - 5 = 2
+        2
+        2 - 2 = 0
+        47
+        3 2 0 1 1 0
+        */
+        System.Console.Write("Nhập vào số tiền cần qui đổi: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        //197 / 50 = 3
+        int t50 = m / 50;
+        //197 - 3 * 50 = 47
+        m %= 50;
+        //47 / 20 = 2
+        int t20 = m / 20;
+        //47 - 2 * 20 = 7
+        m %= 20;
+        int t10 = m / 10;
+        m %= 10;
+        int t5 = m / 5;
+        m %= 5;
+        int t2 = m / 2;
+        m %= 2;
+        int t1 = m / 1;
+        m %= 1;
+        System.Console.Write($"{t50} {t20} {t10} {t5} {t2} {t1}");
+    }
+}
